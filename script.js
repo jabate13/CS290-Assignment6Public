ac0fa808b3dfa90ecd3894dcffb7f83d22fb6ad6
jabate13 <39861552+jabate13@users.noreply.github.com>
2020-06-05 19:50:57 -0700

    Major changes to everything

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //Pretty much just make the row with the
 
 const portNumber = 4852
-const baseUrl = `http://flip3.engr.oregonstate.edu:` + portNumber + `/`
+const baseUrl = `http://flip3.engr.oregonstate.edu:` + portNumber
 
 const deleteTable = function() {
     //Deletes the workoutTable
@@ -22,7 +22,6 @@ const makeTable = function(dataTable){
   makeHeaderRow();
 
   //Iterate over the rows
-  console.log(dataTable.results.length)
   for (var i=0; i < dataTable.results.length; i++) {
     // Create the row
     makeRow(dataTable.results[i]);
@@ -50,6 +49,10 @@ const makeHeaderRow = function() {
     var th = document.createElement('th');
     //console.log(headerData[i]);
     th.innerHTML = headerData[i];
+
+    if ( i == 0 ) {
+      th.classList.add("idTable");
+    }
     //console.log(th)
     row.append(th);
   };
@@ -60,7 +63,7 @@ const makeHeaderRow = function() {
 
 const makeRow = function(row, headerRow = false) {
   // Use the data in the row to create the tableData cells
-  console.log(row);
+  //console.log(row);
   var {id, name, reps, weight, lbs, date } = row;
 
   var tableBody = document.querySelector('tbody');
@@ -68,10 +71,15 @@ const makeRow = function(row, headerRow = false) {
   var new_row = tableBody.insertRow();
 
   console.log(row.date);
-  console.log(typeof(row.date));
+
+  row.date = row.date.substring(0, 10);
 
   for(const property in row) {
     var td = document.createElement('td');
+
+    if (property == 'id') {
+      td.classList.add("idTable");
+    }
 
     if (property == 'lbs') {
       if (row[property] == 1) {
@@ -87,11 +95,11 @@ const makeRow = function(row, headerRow = false) {
   };
 
   var buttonUpdate = document.createElement('td');
-  buttonUpdate.innerHTML = "<button type='button'>Update</button>";
+  buttonUpdate.innerHTML = "<button class='updateButton' type='button'>Update</button>";
   new_row.append(buttonUpdate);
 
   var buttonDelete = document.createElement('td');
-  buttonDelete.innerHTML = "<button type'button'>Delete</button>";
+  buttonDelete.innerHTML = "<button class='deleteButton' type'button'>Delete</button>";
   new_row.append(buttonDelete);
 
   //for( var i=0; i < row)
@@ -117,24 +125,31 @@ const getData = function() {
   })
 };
 
+// Close the popup with the X button
+document.querySelector('.closeButton').onclick = function(event) {
+
+  var popup = document.getElementById('popup');
+  var overlay = document.getElementById('overlay');
+
+  popup.classList.remove('active');
+  overlay.classList.remove('active');
+
+}
+
 //Submit the add form and rebuild the table
 document.querySelector('#addButton').onclick = async function(event) {
   //event listener for the add form button
   let target = event.target;
   event.preventDefault();
 
+  // Capture all the information from the form to send to the server
   var inputName = document.getElementById('inputName').value;
   var inputReps = document.getElementById('inputReps').value;
   var inputWeight = document.getElementById('inputWeight').value;
-  var inputUnits = document.querySelector('input[name="units"]:checked').value
+  var inputUnits = document.querySelector('input[name="units"]:checked').value;
   var inputDate = document.getElementById('inputDate').value;
 
-  console.log(inputName);
-  console.log(inputReps);
-  console.log(inputWeight);
-  console.log(inputUnits);
-  console.log(inputDate);
-
+  // Send the request to the server via POST
   var req = new XMLHttpRequest();
   var payload = { name:inputName, reps:inputReps, weight:inputWeight,
                   lbs:inputUnits, date:inputDate};
@@ -151,9 +166,6 @@ document.querySelector('#addButton').onclick = async function(event) {
 
   req.send(JSON.stringify(payload));
 
-
-
-  console.log('Button stuff');
   // Send the query to the database to get all data
   let dataTable = await getData();
   deleteTable();
@@ -165,47 +177,139 @@ document.querySelector('#workoutTable').onclick = async function(event) {
   //event listener for the update or delete buttons
   let target = event.target;
 
-  //Send the delete request
-  // if (target.tagName == 'btn') {
-  //   var req = new XMLHttpRequest();
-  //   var payload = { id:null };
-  //   payload.id = target.parentNode.parentNode.firstChild.value;
-  //
-  //   req.open('DELETE', 'baseUrl', true);
-  //   req.setRequestHeader('content-type', 'application.json');
-  //   req.addEventListener('load', function(){
-  //     if (req.status >= soo && req.status < 400) {
-  //       var response = JSON.parse(req.respnseText);
-  //       console.log(response);
-  //     } else {
-  //       console.log("Error in network request: " + req.statusText);
-  //     }
-  //   });
-  //   req.send(JSON.stringify(payload));
-  //   event.preventDefault();
-  // }
+  // Send the request to delete a row if the Delete button is pressed
+  if(target.className == 'deleteButton') {
+    console.log('Pushed the delete button');
 
-  //Send the update request
-    //Open the popup form for the update request
+    // get the id of the row to send to the server
+    var deleteId = target.parentNode.parentNode.firstChild.textContent;
 
-  // Send the query to the database to get all data and build the table
-  let dataTable = await getData();
-  deleteTable();
-  console.log('Button pressed');
-  makeTable(dataTable);
+    var req = new XMLHttpRequest();
+    var payload = { id:deleteId };
+
+    req.open('DELETE', baseUrl, true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load', function() {
+      if(req.status >= 200 && req.status < 400) {
+        var response = JSON.parse(req.responseText);
+        console.log(response);
+      } else {
+        console.log('Error in the network request ' + req.statusText);
+      }});
+
+    req.send(JSON.stringify(payload));
+
+    let dataTable = await getData();
+    console.log('finshed the get')
+    deleteTable();
+    makeTable(dataTable);
+  };
+
+
+  // Handler for the Update button/feature
+  if (target.className == 'updateButton') {
+
+    var popup = document.getElementById('popup');
+    var overlay = document.getElementById('overlay');
+
+    popup.classList.add('active');
+    overlay.classList.add('active');
+
+    var rowId = target.parentNode.parentNode.firstElementChild;
+    var rowName = rowId.nextElementSibling;
+    var rowReps = rowName.nextElementSibling;
+    var rowWeight = rowReps.nextElementSibling;
+    var rowDate = rowWeight.nextElementSibling;
+
+    var rowUnits = rowDate.nextElementSibling;
+
+    if(rowUnits.textContent == 'Kg') {
+      document.getElementById('updatekg').checked = true;
+    } else {
+      document.getElementById('updatelbs').checked = true;
+    };
+
+     console.log(rowId.textContent, rowName.textContent, rowWeight.textContent,
+               rowReps.textContent, rowUnits.textContent, rowDate.textContent );
+
+    document.getElementById('updateName').value = rowName.textContent;
+    document.getElementById('updateReps').value = rowReps.textContent;
+    document.getElementById('updateWeight').value = rowWeight.textContent;
+    document.getElementById('updateUnits').value = rowUnits.textContent;
+    document.getElementById('updateDate').value = rowDate.textContent.substring(0, 10);
+
+    document.querySelector('#updateFormButton').onclick = async function(event) {
+      //event listener for the update or delete buttons
+      let target = event.target;
+      event.preventDefault();
+
+      rowId = rowId.textContent;
+      rowName = document.getElementById('updateName').value;
+      rowReps = document.getElementById('updateReps').value;
+      rowWeight = document.getElementById('updateWeight').value;
+      rowUnits = document.querySelector('input[name="updateUnits"]:checked').value;
+      rowDate = document.getElementById('updateDate').value;
+
+      console.log(rowId, rowName, rowReps, rowWeight, rowUnits, rowDate );
+
+      var req = new XMLHttpRequest();
+      var payload = { id:rowId, name:rowName, reps:rowReps,
+                      weight:rowWeight, lbs:rowUnits, date:rowDate };
+
+      req.open('PUT', baseUrl, true);
+      req.setRequestHeader('Content-Type', 'application/json');
+      req.addEventListener('load', function() {
+        if(req.status >= 200 && req.status < 400) {
+          var response = JSON.parse(req.responseText);
+          console.log(response);
+        } else {
+          console.log('Error in the network request ' + req.statusText);
+        }});
+
+      req.send(JSON.stringify(payload));
+
+      let dataTable = await getData();
+      console.log('finshed the get')
+      deleteTable();
+      makeTable(dataTable);
+
+      var popup = document.getElementById('popup');
+      var overlay = document.getElementById('overlay');
+    
+      popup.classList.remove('active');
+      overlay.classList.remove('active');
+
+  };
+
+
 };
 
-const table = document.getElementById('workoutTable');
-table.addEventListener('click', function(event){
-  let target = event.target; // Grab the element that actually sends the event
+};
 
-  // If update button send a put request to the server
+document.querySelector('#updateFormButton').onclick = async function(event) {
+  //event listener for the update or delete buttons
+  let target = event.target;
+  event.preventDefault();
 
-  //If its a delete button send a delete request to the server
 
-  // Delete the table
-  // Make the table again
-});
+  // get the id of the row to send to the server
+  var rowId = target.parentNode.parentNode.firstElementChild;
+  var rowName = rowId.nextElementSibling;
+  var rowReps = rowName.nextElementSibling;
+  var rowWeight = rowReps.nextElementSibling;
+  var rowDate = rowWeight.nextElementSibling;
+
+  var rowUnits = rowDate.nextElementSibling;
+
+  console.log(rowId.textContent, rowName.textContent, rowWeight.textContent,
+            rowReps.textContent, rowUnits.textContent, rowDate.textContent );
+
+  var req = new XMLHttpRequest();
+  var payload = { id:deleteId };
+
+
+
+  };
 
 // Runs as soon as the page loads, intial get request
 const pageLoad = async function() {
